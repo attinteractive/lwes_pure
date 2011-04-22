e6ad354669be783d96cc12ee9b18bd3a17ef32f5
Aaron Qian <aqian@attinteractive.com>
2011-04-21 18:51:10 -0700

    documented all the public methods and classes

--- lib/lwes/emitter.rb
+++ lib/lwes/emitter.rb
@@ -1,6 +1,7 @@
 require 'socket'
 
 module Lwes
+  # Emit LWES events to a UDP endpoint
   class Emitter
     DEFAULT_SOCIKET_OPTIONS = {
       :address_family => Socket::AF_INET,
@@ -9,13 +10,21 @@ module Lwes
     }
     
     attr_accessor :socket_options, :socket
-
+    
+    # Creates a new {Lwes::Emitter}.
+    # @param [Hash] options
+    # @option options [String] :host UDP host address, defaults to 127.0.0.1
+    # @option options [String, Number] :port UDP port, defaults to 12345
+    # @option options [Number] :address_family, Address family of the socket, defaults to Socket::AF_INET
     def initialize(options={})
       self.socket_options = DEFAULT_SOCIKET_OPTIONS.merge(options)
       self.socket = UDPSocket.new(@socket_options[:address_family])
       socket.connect(@socket_options[:host], @socket_options[:port])
     end
 
+    # Emits specified event
+    # @param [Lwes::Event] event The event to send
+    # @return [Number] the number of bytes sent
     def emit(event)
       buffer = StringIO.new
       event.write(buffer)
--- lib/lwes/event.rb
+++ lib/lwes/event.rb
@@ -4,37 +4,48 @@ require 'lwes/helpers'
 require 'lwes/serialization'
 
 module Lwes
+  # Represent a LWES event, can read from and write to IO objects
   class Event
     attr_accessor :name, :attributes
+    
+    # Creates a new {Lwes::Emitter}.
+    # @param [Hash] hash
+    # @option hash [String] :name The event name
+    # @option hash [Hash] :attributes Attributes of the event.
+    #                                 The hash key is the attribute name, the hash value should be an Array of two elements.
+    #                                 The first element is the type see {Lwes::TYPE_TO_BYTE} for all available types.
+    #                                 The second element is the actual value of the attribute.
+    # @option hash [Number] :address_family, Address family of the socket, defaults to Socket::AF_INET.
     def initialize(hash={})
       self.name = hash[:name] || ""
       self.attributes = hash[:attributes] || {}
     end
-    
-    def name=(val)
-      @name = val
-    end
 
-    def attributes=(val)
-      @attributes = val
-    end
-    
+    # Writes the event to an IO object
+    # @param [IO] io The IO object to write to.
+    # @return [Number] Number of bytes written to IO
     def write(io)
       serializer.write(io)
     end
-    
+
+    # Read from an IO object
+    # @param [IO] io The IO object to read from.
     def read(io)
       serializer = Lwes::Serialization::Event.read(io)
       set_attributes_from_serializer(serializer)
-      self
     end
     
+    # Read from an IO object
+    # @param [IO] io The IO object to read from.
+    # @return [Lwes::Event] a new {Lwes::Event} instance with data filled from the IO.
     def self.read(io)
       event = new
       event.read(io)
       event
     end
     
+    # Converts the event to a hash representation
+    # @return [Hash] the hash representation of the instance.
     def to_hash
       {
         :name => name,
