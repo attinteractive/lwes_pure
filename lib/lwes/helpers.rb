e33fb3af3eeb85d74d5621c8115e2322698ed27b
Aaron Qian <aqian@attinteractive.com>
2011-04-21 18:10:55 -0700

    update docs

--- lib/lwes/helpers.rb
+++ lib/lwes/helpers.rb
@@ -1,5 +1,6 @@
 module Lwes
   module Helpers
+    # converts snake cased strings to camelcase
     def camelcase(str)
       str = str.to_s
       str.gsub(/^[a-z]|_+[a-z]/){|str| str.upcase}.gsub("_", '')
